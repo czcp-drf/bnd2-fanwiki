64aeaff1810ca035dc140ee15beaae1ddbd771bc
czcp-drf <rmh001005@gmail.com>
2026-09-11 20:20:55 +0900

    style: customize map switcher to match site theme

--- src/components/map/MapBaseLayers.tsx
+++ src/components/map/MapBaseLayers.tsx
@@ -2,14 +2,15 @@
 
 import { LayersControl, TileLayer } from 'react-leaflet'
 import { MAP_TILE_URLS } from '@/lib/map/constants'
+import './MapBaseLayers.css'
 
 export default function MapBaseLayers() {
   return (
     <LayersControl position="topright" collapsed={false}>
-      <LayersControl.BaseLayer checked name="Atlas (일반 지도)">
+      <LayersControl.BaseLayer checked name="Atlas">
         <TileLayer url={MAP_TILE_URLS.atlas} noWrap />
       </LayersControl.BaseLayer>
-      <LayersControl.BaseLayer name="Satellite (위성 지도)">
+      <LayersControl.BaseLayer name="위성">
         <TileLayer url={MAP_TILE_URLS.satellite} noWrap />
       </LayersControl.BaseLayer>
     </LayersControl>
